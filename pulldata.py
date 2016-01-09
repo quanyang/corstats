efb0f5ea0f158ea24a2006845595317e0d9b88b3
Quan Yang <quanyang@live.com>
2016-01-09 21:48:07 +0800

    Got all data, now to make sense of it

--- pulldata.py
+++ pulldata.py
@@ -15,7 +15,8 @@ pulls data of all rounds from cors
 __author__ = "QuanYang"
 __version__ = "0.1"
 
-import os, sys, requests,re
+import os, sys, requests, re, json
+from pprint import pprint
 
 class CorsData:
 
@@ -25,11 +26,19 @@ class CorsData:
 		self.request = requests.session()
 		self.all_Modules = dict()
 
+		self.parseCorsSchedule()
+		
 		#	populate variables
-		self.getAllOpenBid()
+		self.getAllOpenBidStats()
+		self.getAllCloseBidStats()
+		self.getAllBiddingSummary()
 		self.getAllModules()
 
-	
+
+	def parseCorsSchedule(self):
+		filename = "corsSchedule_%s.json"%self.semester
+		schedule = file(filename,"rb")
+		self.schedule = json.loads(schedule.read())
 
 	def getAllModules(self):
 		url = "https://myaces.nus.edu.sg/cors/jsp/report/ModuleInfoListing.jsp"
@@ -48,7 +57,7 @@ class CorsData:
 				self.modules[module_Code] = data_split[2:]
 
 	def getOpenBid(self,round,buffer):
-		url = "http://www.cors.nus.edu.sg/Reports/%s_%s_%s.html"%("openbid",round,self.semester)
+		url = "http://www.cors.nus.edu.sg/Reports/openbid_%s_%s.html"%(round,self.semester)
 		r = self.request.get(url);
 		if r.status_code == 200:
 			data = r.text.strip().replace("  ",'').replace("\r\n","")
@@ -68,7 +77,88 @@ class CorsData:
 						buffer[key] = int(data_split[2])
 					prevmod = key
 
-	def getAllOpenBid(self):
+	def getCloseBid(self,round,buffer):
+		closingTime = "1300"
+		for data in self.schedule:
+			if round == data['round']:
+				# becomes 13:00:00
+				closingTime = data['openBiddingEnd'][-8:].replace(":","")[0:4]
+
+		url = "http://www.cors.nus.edu.sg/Reports/closebidinfo_%s_%s_%s.html"%(round,closingTime,self.semester)
+		r = self.request.get(url)
+		if r.status_code == 200:
+			data = r.text.strip().replace("  ",'').replace("\r\n","").replace("\t","")
+			results = re.findall(r'<tr valign=top bgcolor=\#[a-f0-9A-F]{6}>(.*?)</tr>',data)
+			prevmod = "";
+			for result in results:
+				if "<td colspan=2>" in result:
+					#belongs to prev mod
+					data_split = re.findall(r'<td><p>(\d+?)</p></td>',result)
+					buffer[prevmod][0] += int(data_split[1])
+					buffer[prevmod][1] += int(data_split[2])
+				else:
+					data_split = result.replace("</p></td>","").split("<td><p>")[1:]
+					key = (data_split[0],data_split[1])
+					if key in buffer:
+						buffer[key][0] += int(data_split[4])
+						buffer[key][1] += int(data_split[5])
+					else:
+						buffer[key] = [int(data_split[4]),int(data_split[5])]
+					prevmod = key
+
+	def getSummary(self,round,buffer):
+		url = "http://www.cors.nus.edu.sg/Reports/successbid_%s_%s.html"%(round,self.semester)
+		r = self.request.get(url)
+		if r.status_code == 200:
+			data = r.text.strip().replace("  ",'').replace("\r\n","").replace("\t","")
+			results = re.findall(r'<tr valign=top bgcolor=\#[a-f0-9A-F]{6}>(.*?)</tr>',data)
+			prevmod = "";
+			for result in results:
+				if "<td colspan=2>" in result:
+					#belongs to prev mod
+					data_split = re.findall(r'<td><p>(\d+?)</p></td>',result)
+					buffer[prevmod] += int(data_split[1])
+				else:
+					data_split = result.replace("</p></td>","").split("<td><p>")[1:]
+					key = (data_split[0],data_split[1])
+					if key in buffer:
+						buffer[key] += int(data_split[3])
+					else:
+						buffer[key] = int(data_split[3])
+					prevmod = key
+
+
+
+	def getAllBiddingSummary(self):
+		round1A_Summary = dict()
+		self.getSummary("1A",round1A_Summary)
+		round1B_Summary = dict()
+		self.getSummary("1B",round1B_Summary)
+		round2A_Summary = dict()
+		self.getSummary("2A",round2A_Summary)
+		round2B_Summary = dict()
+		self.getSummary("2B",round2B_Summary)
+		round3A_Summary = dict()
+		self.getSummary("3A",round3A_Summary)
+		round3B_Summary = dict()
+		self.getSummary("3B",round3B_Summary)
+
+
+	def getAllCloseBidStats(self):
+		round1A_CloseBid = dict()
+		self.getCloseBid("1A",round1A_CloseBid)
+		round1B_CloseBid = dict()
+		self.getCloseBid("1B",round1B_CloseBid)
+		round2A_CloseBid = dict()
+		self.getCloseBid("2A",round2A_CloseBid)
+		round2B_CloseBid = dict()
+		self.getCloseBid("2B",round2B_CloseBid)
+		round3A_CloseBid = dict()
+		self.getCloseBid("3A",round3A_CloseBid)
+		round3B_CloseBid = dict()
+		self.getCloseBid("3B",round3B_CloseBid)
+
+	def getAllOpenBidStats(self):
 		round1A_OpenBid = dict()
 		self.getOpenBid("1A",round1A_OpenBid)
 		round1B_OpenBid = dict()
